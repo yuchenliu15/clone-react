371e6f3724fd2895a231a16dc153a63c7ac726e1
Yuchen <yl6448@nyu.edu>
2020-06-12 13:56:51 -0700

    finish email

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import EmailForm from '../EmailForm';
+import Grid from '@material-ui/core/Grid';
 
 const firstHead = "Build a landing page for your business or project and generate more leads!";
 
@@ -8,9 +9,10 @@ function App() {
 	return (
 		<div className="App">
 			<EmailForm title={firstHead} />
+
 		</div>
 	);
-	
+
 }
 
 export default App;
--- src/components/EmailForm/index.js
+++ src/components/EmailForm/index.js
@@ -1,6 +1,17 @@
-import React, {useState} from 'react';
-import {connect} from 'react-redux';
-import {doAddEmail} from '../../actions';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { doAddEmail } from '../../actions';
+import { Button, TextField } from '@material-ui/core';
+import { blue } from '@material-ui/core/colors';
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import styles from './form.module.css';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blue,
+    },
+});
+
 
 const EmailForm = ({ title, onAddEmail }) => {
 
@@ -17,13 +28,26 @@ const EmailForm = ({ title, onAddEmail }) => {
     }
 
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <label><h1>{title}</h1></label>
-                <input placeholder="Enter your email..." value={text} onChange={onChange} />
-                <button type="submit">Sign up!</button>
+        <div className={styles.container}>
+            <div className={styles.content}>
+            <form className={styles.form} onSubmit={onSubmit}>
+                <label><h1 className={styles.text}>{title}</h1></label>
+                <TextField className={styles.input} placeholder="Enter your email..." value={text} onChange={onChange} />
+                <SubmitButton onSubmit={onSubmit} />
             </form>
+            </div>
+
         </div>
+    )
+}
+
+const SubmitButton = ({ onSubmit }) => {
+    return (
+        <ThemeProvider theme={theme}>
+            <Button variant="contained" color="primary" disableElevation type="submit" onSubmit={onSubmit}>
+                Sign Up!
+            </Button>
+        </ThemeProvider>
     )
 }
 
