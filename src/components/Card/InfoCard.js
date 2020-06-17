f3f695e09f64ce61cbb57b17759cdc4b3152f563
Yuchen <yl6448@nyu.edu>
2020-06-17 09:44:45 -0700

    hover effect

--- src/components/Card/InfoCard.js
+++ src/components/Card/InfoCard.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import styles from './card.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useSpring, animated } from 'react-spring'
+
 
 const InfoCard = ({ props }) => {
-
+    const [motion, set] = useSpring(() => ({ fontSize: "5em" }));
+    const AnimatedComponent = animated(FontAwesomeIcon);
     const {
         icon,
         title,
         info
     } = props;
 
+    const onMouseEnter = (event) => {
+        set({fontSize: "6em"});
+    }
+
+    const onMouseLeave = (event) => {
+        set({fontSize: "5em"});
+    }
+
     return (
         <div key={title} className={styles.card}>
-            <FontAwesomeIcon icon={icon} size="5x" />
+            <AnimatedComponent icon={icon} style={motion} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} />
             <h3>{title}</h3>
             <p>{info}</p>
         </div>
