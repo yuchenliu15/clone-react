c8b2ffceb1a06d13fa87a20a7c8449e9d9c45533
Yuchen <yl6448@nyu.edu>
2020-06-19 17:05:53 -0700

    add listener

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import InfoCard from './InfoCard';
 import PersonCard from './PersonCard';
 import styles from './card.module.css';
+import {useSpring} from 'react-spring';
 
 const withCardsCollection = (Component, title) => ({ cards }) => {
+
+    const ref = useRef();
+
+    const handleScroll = () => {
+        const posY = ref.current.getBoundingClientRect().top;
+        console.log(posY);
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    });
+
     return (
-        <div className={styles.cardContainer}>
+        <div className={styles.cardContainer} ref={ref} >
             {title ? <div className={styles.title}><h2>{title}</h2></div> : null}
             {cards.map((card, index) => {
                 return <Component key={index} props={card} />
